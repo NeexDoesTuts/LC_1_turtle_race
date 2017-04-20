f45c09a3e82d86fdb261101132b4571d7c1be6ad
Neex.IO <neex-io@users.noreply.github.com>
2017-04-19 23:11:37 -0700

    Add game stop on window bounds.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import turtle
 import random
 
 def coinToss():
-    return random.randrange(0,2,1)
+    return random.randrange(0,2)
 
 def moveTurtle(turtle, step):
     direction = coinToss()
@@ -12,7 +12,32 @@ def moveTurtle(turtle, step):
         turtle.left(90)
     turtle.forward(step)
 
+def isOnScreen(window,turtle):
+    # minus step - so the turtle does not disappear. if it hits the wall, it is done
+    leftBound = -(window.window_width() / 2)
+    rightBound = window.window_width() / 2
+    topBound = window.window_height() / 2
+    bottomBound = -(window.window_height() / 2)
+    print(leftBound, rightBound, topBound, bottomBound)
+
+    turtleX = turtle.xcor()
+    turtleY = turtle.ycor()
+
+    exit_x = True
+    exit_y = True
+    if turtleX > rightBound or turtleX < leftBound:
+        exit_x = False
+    if turtleY > topBound or turtleY < bottomBound:
+        exit_y = False
+
+    return exit_y and exit_x
+
+# in stead of checking the window size, I am setting it so it does not take the automatic 0.5 when I have two screens and becomes HUGE
+
 window = turtle.Screen()
+window_height = 600
+window_width = 1000
+window.setup(window_width, window_height)
 
 fiat = turtle.Turtle()
 skoda = turtle.Turtle()
@@ -22,10 +47,11 @@ fiat.speed(10)
 skoda.color("blue")
 skoda.speed(10)
 
-temp_wall = 0
-while temp_wall < 100:
-    temp_wall += 1
-    moveTurtle(fiat, 10)
-    moveTurtle(skoda, 10)
+turtlesOnScreen = isOnScreen(window,fiat) and isOnScreen(window, skoda)
+
+while turtlesOnScreen:
+    moveTurtle(fiat, 30)
+    moveTurtle(skoda, 30)
+    turtlesOnScreen = isOnScreen(window,fiat) and isOnScreen(window, skoda)
 
 window.exitonclick()
